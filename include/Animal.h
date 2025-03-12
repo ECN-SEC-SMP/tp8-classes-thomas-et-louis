09294cf3c18cb8d85216f5b96d969ff1bc5bf1d4
Thomas Verron <verronthomas03@gmail.com>
2025-03-12 09:15:50 +0100

    Class Animal

---
+++ include/Animal.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include "Attaque.h"
+
+
+class Animal
+{
+protected :
+    std::string nom;
+    int x;
+    int y;
+    bool vivant;
+    Attaque typeAttaque;
+public:
+    Animal(int maxX, int maxY);
+    Animal(int maxX, int maxY, int x, int y);
+    std::string getNom() const;
+    int getX() const;
+    int getY() const;
+    bool getVivant() const;
+    Attaque getAttaque() const;
+    void setVivant(bool v);
+    bool attaque(Animal &a);
+    virtual void setAttaque() = 0; //virtuelle pure
+    virtual void deplace(int maxX, int maxY) = 0; // virtuelle pure
+};
---
+++ src/Animal.cpp
@@ -0,0 +1,60 @@
+#include "Animal.h"
+
+
+#include <stdlib.h>
+#include <time.h>
+
+
+Animal::Animal(int maxX, int maxY)
+{
+    srand(time(NULL));
+    x = rand() % maxX;
+    y = rand() % maxY;
+}
+
+Animal::Animal(int maxX, int maxY, int x, int y)
+    :x(x), y(y)
+{
+    if(this->x > maxX){
+        this->x = maxX;
+    }
+    if(this->y > maxY){
+        this->y = maxY;
+    }    
+}
+
+std::string Animal::getNom() const
+{
+    return nom;
+}
+
+int Animal::getX() const
+{
+    return x;
+}
+
+int Animal::getY() const
+{
+    return y;
+}
+
+bool Animal::getVivant() const
+{
+    return vivant;
+}
+
+Attaque Animal::getAttaque() const
+{
+    return typeAttaque;
+}
+
+void Animal::setVivant(bool v)
+{
+    this->vivant = v;
+}
+
+bool Animal::attaque(Animal &a)
+{
+    return this->typeAttaque.resoudreAttaque(a.typeAttaque);
+}
+
