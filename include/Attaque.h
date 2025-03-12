5087b8cb450125b73668e07713ebbc30864fbe74
Louis Vaillant <louis.vaillant@eleves.ec-nantes.fr>
2025-03-12 09:13:28 +0100

    Update Attaque

--- include/Attaque.h
+++ include/Attaque.h
@@ -5,12 +5,38 @@
 class Attaque
 {
 private:
-    int typeAttaque; // 0: pierre, 1: papier, 2: ciseaux
+    int typeAttaque; // 0: pierre, 1: feuille, 2: ciseaux
 public:
+    /**
+     * @brief Construct a new Attaque object
+     * @details Attaque type set randomly
+     */
     Attaque();
-    Attaque(int a);
-    ~Attaque();
+    /**
+     * @brief Construct a new Attaque object
+     * 
+     * @param type (IN) Attaque type (0: pierre, 1: feuille, 2: ciseaux)
+     */
+    Attaque(int type);
+    ~Attaque() = default;
+    /**
+     * @brief Get the Attaque Type
+     * 
+     * @return int Attaque type (0: pierre, 1: feuille, 2: ciseaux)
+     */
     int getTypeAttaque() const;
+    /**
+     * @brief Process Attaque
+     * 
+     * @param a 
+     * @return true if win
+     * @return false else
+     */
     bool resoudreAttaque(Attaque &a) const;
+    /**
+     * @brief Get the Attaque Name 
+     * 
+     * @return std::string Attaque Name 
+     */
     std::string getNomAttaque() const;
 };
--- src/Attaque.cpp
+++ src/Attaque.cpp
@@ -6,30 +6,42 @@
 Attaque::Attaque()
 {
     srand(time(NULL));
-    
+    this->typeAttaque = rand() % 3;
 }
 
-Attaque::Attaque(int a)
+Attaque::Attaque(int type)
 {
-
-}
-
-Attaque::~Attaque()
-{
-
+    this->typeAttaque = type;
 }
 
 int Attaque::getTypeAttaque() const
 {
-
+    return this->typeAttaque;
 }
 
 bool Attaque::resoudreAttaque(Attaque &a) const
 {
-
+    int typeAtt = a.getTypeAttaque();
+    return (this->typeAttaque == 0 && typeAtt == 2) 
+        || (this->typeAttaque == 1 && typeAtt == 0) 
+        || (this->typeAttaque == 2 && typeAtt == 1);
 }
 
 std::string Attaque::getNomAttaque() const
 {
+    switch (this->typeAttaque)
+    {
+    case 0:
+        return "Piere";
+        break;
+    case 1:
+        return "Feuille";
+        break;
+    case 2:
+        return "Ciseaux";
+        break;
 
+    default:
+        break;
+    }
 }
