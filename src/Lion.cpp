13fd605d622e2c74701c39a21707c0b0bd782506
Louis Vaillant <louis.vaillant@eleves.ec-nantes.fr>
2025-03-12 09:38:56 +0100

    Update Lion

--- src/Lion.cpp
+++ src/Lion.cpp
@@ -1,17 +1,30 @@
 #include "Lion.h"
 
+#include <stdlib.h>
+#include <time.h>
+
 Lion::Lion(int maxX, int maxY)
     : Animal(maxX, maxY)
 {
-    
+    this->nom = "L";
+    this->setVivant(true);
 }
 
-void setAttaque()
+void Lion::setAttaque()
 {
-
+    int att_type = rand() % 2 + 1;
+    this->typeAttaque = Attaque(att_type);
 }
 
-void deplace(int maxX, int maxY)
+void Lion::deplace(int maxX, int maxY)
 {
-    
+    srand(time(NULL));
+    int dx = rand() % 2 ? 1 : -1;
+    int dy = rand() % 2 ? 1 : -1;
+    this->x += dx;
+    this->y += dy;
+    if (this->x > maxX) this->x %= maxX;
+    if (this->y > maxY) this->y %= maxY;
+    if (this->x < 0) this->x += maxX;
+    if (this->y < 0) this->y += maxY;
 }
